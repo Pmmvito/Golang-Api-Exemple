7fe120ebaed043b8abcbd60e5c274efc3235162e
Vitor <pmmvito@hotmail.com>
2025-09-07 19:17:21 -0300

    feat: update handler structure to separate package

--- router/routes.go
+++ router/routes.go
@@ -2,37 +2,23 @@ package router
 
 import (
 	"net/http"
+
+	"github.com/Pmmvito/Golang-Api-Exemple/handler"
 	"github.com/gin-gonic/gin"
 )
 
 func InitializeRoutes(router *gin.Engine){
 	v1 := router.Group("/api/v1")
 	{
-		v1.GET("/opening", func (ctx *gin.Context)  {
-			ctx.JSON(http.StatusOK, gin.H{
-		    "msg": "Helloowww",	
-			})
-		})
-		v1.POST("/opening", func (ctx *gin.Context)  {
-			ctx.JSON(http.StatusOK, gin.H{
-		    "msg": "Helloowww",	
-			})
-		})
-		v1.DELETE("/opening", func (ctx *gin.Context)  {
-			ctx.JSON(http.StatusOK, gin.H{
-		    "msg": "Delete opening",	
-			})
-		})
-		v1.PUT("/opening", func (ctx *gin.Context)  {
-			ctx.JSON(http.StatusOK, gin.H{
-		    "msg": "Put opening",	
-			})
-		})
-		v1.GET("/openings", func (ctx *gin.Context)  {
-			ctx.JSON(http.StatusOK, gin.H{
-		    "msg": "Get opening",	
-			})
-		})
+		v1.GET("/opening", handler.ShowOpeningHandler)
+
+		v1.POST("/opening", handler.CreateOpeningHandler)
+
+		v1.DELETE("/opening", handler.DeleteOpeningHandler)
+
+		v1.PUT("/opening", handler.UpdateOpeningHandler)
+		
+		v1.GET("/openings", handler.ListOpeningHandler)
 	
 	}
 }
