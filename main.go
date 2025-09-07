561c73272916a8d7054d47d9bcf9ab4e3269e83c
Vitor <pmmvito@hotmail.com>
2025-09-07 20:23:29 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,8 +1,26 @@
 package main
 
-import "github.com/Pmmvito/Golang-Api-Exemple/router"
+import (
+	
+
+	"github.com/Pmmvito/Golang-Api-Exemple/config"
+	"github.com/Pmmvito/Golang-Api-Exemple/router"
+)
+
+var(
+	logger *config.Logger
+)
 
 func main() {
+
+	logger = config.GetLogger("main")
+
+	//initialize configs of projects\
+	err := config.Init()
+	if err != nil{
+		logger.ErrorF("config initialization erro: %v",err)
+		return
+	}
 
 	router.Initialize()
 }
